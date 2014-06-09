8cf9533673b8af316991ac4cdf5d870885b4d8e0
David Alpert <david@spinthemoose.com>
2014-06-08 21:45:56 -0500

    refactoring

---
+++ BowlingCalculator.Tests/Steps/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingCalculator.Tests.Steps
+{
+    internal static class EnumerableExtensions
+    {
+        internal static void ForEach<T>(this IEnumerable<T> items, Action<T> actOn)
+        {
+            if (actOn == null) return;
+            items.ForEach((index, item) => actOn(item));
+        }
+
+        internal static void ForEach<T>(this IEnumerable<T> items, Action<int,T> actOn)
+        {
+            if (actOn == null) return;
+            var index = 0;
+            foreach (var item in items)
+            {
+                actOn(index, item);
+                index++;
+            }
+        }
+    }
+}
--- BowlingCalculator.Tests/Steps/GameSteps.cs
+++ BowlingCalculator.Tests/Steps/GameSteps.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
@@ -7,34 +6,37 @@ using TechTalk.SpecFlow.Assist;
 
 namespace BowlingCalculator.Tests.Steps
 {
-    public static class EnumerableExtensions
-    {
-        public static void ForEach<T>(this IEnumerable<T> items, Action<T> actOn)
-        {
-            if (actOn == null) return;
-            items.ForEach((index, item) => actOn(item));
-        }
-
-        public static void ForEach<T>(this IEnumerable<T> items, Action<int,T> actOn)
-        {
-            if (actOn == null) return;
-            var index = 0;
-            foreach (var item in items)
-            {
-                actOn(index, item);
-                index++;
-            }
-        }
-    }
-
     [Binding]
-    public class CreateGameSteps
+    public class GameSteps
     {
         [Given(@"a game has been set up with the following players:")]
         public void GivenAGameHasBeenSetUpWithTheFollowingPlayers(Table table)
         {
             GivenICreateAGameWithPlayer(table.RowCount);
             GivenIAddTheFollowingPlayersToTheGame(table);
+        }
+
+        [Given(@"I create a game with (.*) players?")]
+        public void GivenICreateAGameWithPlayer(int numberOfPlayers)
+        {
+            var game = new BowlingGame(numberOfPlayers);
+            ScenarioContext.Current.Set(game);
+        }
+        
+        [Given(@"I add the following players to the game:")]
+        public void GivenIAddTheFollowingPlayersToTheGame(Table table)
+        {
+            var names = table.Rows.Select(r => r.Values.ElementAt(0).Trim());
+            var game = ScenarioContext.Current.Get<BowlingGame>();
+            names.ForEach(game.SetPlayerName);
+        }
+        
+        [When(@"I list the players")]
+        public void WhenIListThePlayers()
+        {
+            var game = ScenarioContext.Current.Get<BowlingGame>();
+            var list = game.ListPlayers();
+            ScenarioContext.Current.Set(list, "player list");
         }
         
         [When(@"I press start")]
@@ -70,29 +72,6 @@ namespace BowlingCalculator.Tests.Steps
             Assert.AreEqual(ballNumber, game.CurrentBall);
         }
 
-        [Given(@"I create a game with (.*) players?")]
-        public void GivenICreateAGameWithPlayer(int numberOfPlayers)
-        {
-            var game = new BowlingGame(numberOfPlayers);
-            ScenarioContext.Current.Set(game);
-        }
-        
-        [Given(@"I add the following players to the game:")]
-        public void GivenIAddTheFollowingPlayersToTheGame(Table table)
-        {
-            var names = table.Rows.Select(r => r.Values.ElementAt(0).Trim());
-            var game = ScenarioContext.Current.Get<BowlingGame>();
-            names.ForEach(game.SetPlayerName);
-        }
-        
-        [When(@"I list the players")]
-        public void WhenIListThePlayers()
-        {
-            var game = ScenarioContext.Current.Get<BowlingGame>();
-            var list = game.ListPlayers();
-            ScenarioContext.Current.Set(list, "player list");
-        }
-        
         [Then(@"the list shall include the following players:")]
         public void ThenTheListShallIncludeTheFollowingPlayers(Table table)
         {
